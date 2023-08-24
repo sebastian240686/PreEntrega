f40d642fa130561d7808f95b6efc3333a6bca602
Sebastian Leguisamo <sleguisamoberri@gmail.com>
2023-08-24 19:34:40 -0300

    camobios

--- desafio2/cypress/e2e/PreEntrega/PreEntrega.cy.js
+++ desafio2/cypress/e2e/PreEntrega/PreEntrega.cy.js
@@ -1,1 +1,40 @@
-///<reference types
+///<reference types = 'cypress' />
+
+import { HomePage } from "../../support/page/homepage";
+import  {LoginPage} from "../../support/page/loginpage";
+import { ProductsPage } from "../../support/page/productsPage";
+
+describe("PreEntrega",() => {
+    let dato;
+    let items;
+    const loginpage = new LoginPage();
+    const homepage = new HomePage();
+    const productsPage = new ProductsPage();
+    before("before",()=>{
+
+     cy.fixture('datos').then(data =>{
+        dato=data;
+     })
+
+     cy.fixture('productos').then(listaDeProductos =>{
+        items=listaDeProductos;
+     })
+    })
+
+    beforeEach("Ingreso de datos validos", () =>{
+
+        cy.visit('');
+        cy.get('#registertoggle').dblclick();
+        loginpage.writeUser(dato.username);
+        loginpage.writePassword(dato.password);
+        loginpage.clickLoginButton();
+        homepage.clickOnlineButton();
+    })
+    it("Actividad Pre Entrega", () =>{
+
+     productsPage.cargarProductos('Black Jacket', 'White Pants');
+
+
+
+    })
+})
--- desafio2/cypress/support/page/productsPage.js
+++ desafio2/cypress/support/page/productsPage.js
@@ -2,15 +2,16 @@ export class ProductsPage{
 
         constructor(){
 
-            this.producto1 = "//p[text()='Black Jacket']/following-sibling::button[text()='Add to cart']";
-            this.producto2 = "//p[text()='White Pants']/following-sibling::button[text()='Add to cart']"            
+            this.producto1 = '//p[text()="Black Jacket"]/following-sibling::button[text()="Add to cart"]';
+            this.producto2 = '//p[text()="White Pants"]/following-sibling::button[text()="Add to cart"]' ;           
         }
 
         cargarProductos(product1,product2){
-                cy.xpath(this.producto1).type(product1);
-                
-                cy.xpath(this.producto2).type(product2);
 
+                cy.xpath(this.producto1).type(product1).click();
+               cy.get('#closeModal');
+               cy.xpath(this.producto2).type(product2).click();
+               cy.get('#closeModal');
         }
 
 }
